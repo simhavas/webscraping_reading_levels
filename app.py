a100c94831b8858ecc6026500916363f6fe610d6
udaysimha <simhavs@gmail.com>
2018-01-27 16:16:53 +0100

    Second commit, added all required files for HEROKU.

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ Created on Fri Jan 26 12:10:00 2018
 """
 
 
-import os, re, string, requests, operator, urllib, math
-from flask import Flask, render_template, request, Markup, jsonify
-#from flask.ext.sqlalchemy import SQLAlchemy
-#from stop_words import stops
+import re, requests, math
+from flask import Flask, request, jsonify
 from collections import Counter
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize, sent_tokenize
